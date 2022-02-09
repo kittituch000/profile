5d211f434ab0c6828d9f18eb214f3b093588d5c2
kitttituch <kittituch@magicboxasia.com>
2022-02-09 20:06:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import nodeImage from './image/node.png'
-import pythonImage from './image/python.png'
-import unityImage from './image/unity.png'
-import gitImage from './image/git.png'
+import nodeImage from './image/node.png';
+import pythonImage from './image/python.png';
+import unityImage from './image/unity.png';
+import gitImage from './image/git.png';
 import './App.css';
 
 function App() {
   return (
-    <div className = "grid-bottom">
+    <div className="grid-bottom">
       <div>
       </div>
-      <div className = "grid-bottom-in">
+      <div className="grid-bottom-in">
         <div>
-          <img   src={nodeImage} width ='600' height = '400'  alt="description of image"/>
+          <img src={nodeImage} className="App-logo" alt="description of image" />
         </div>
         <div>
           <h1>test</h1>
@@ -20,26 +20,26 @@ function App() {
           <h1>test</h1>
         </div>
         <div>
-          <img   src={pythonImage} width ='600' height = '400'  alt="description of image"/>
+          <img src={pythonImage} className="App-logo" alt="description of image" />
         </div>
         <div>
-          <img   src={unityImage} width ='600' height = '400'  alt="description of image"/>
-        </div>
-        <div>
-          <h1>test</h1>
+          <img src={unityImage} className="App-logo" alt="description of image" />
         </div>
         <div>
           <h1>test</h1>
         </div>
         <div>
-          <img   src={gitImage} width ='600' height = '400'  alt="description of image"/>
+          <h1>test</h1>
+        </div>
+        <div>
+          <img src={gitImage} className="App-logo" alt="description of image" />
         </div>
 
 
       </div>
       <div>
       </div>
-      
+
 
     </div>
   )
