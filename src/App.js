103bd7c7bf40cea21e346a026a273c230ac1667c
kitttituch <kittituch@magicboxasia.com>
2022-02-12 11:36:03 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import nodeImage from './image/node.png';
-import pythonImage from './image/python.png';
-import unityImage from './image/unity.png';
-import gitImage from './image/git.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="grid-bottom">
-      <div>
-      </div>
-      <div className="grid-bottom-in">
-        <div>
-          <img src={nodeImage} className="App-logo" alt="description of image" />
-        </div>
-        <div>
-          <h1>test</h1>
-        </div>
-        <div>
-          <h1>test</h1>
-        </div>
-        <div>
-          <img src={pythonImage} className="App-logo" alt="description of image" />
-        </div>
-        <div>
-          <img src={unityImage} className="App-logo" alt="description of image" />
-        </div>
-        <div>
-          <h1>test</h1>
-        </div>
-        <div>
-          <h1>test</h1>
-        </div>
-        <div>
-          <img src={gitImage} className="App-logo" alt="description of image" />
-        </div>
 
-
-      </div>
-      <div>
-      </div>
-
+    <p>sdfsdfsdf</p>
 
     </div>
   )
