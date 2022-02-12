9df21a4ed427f82f9453d9764a552de6e0c91ca8
kitttituch <kittituch@magicboxasia.com>
2022-02-12 11:57:09 +0700

    add image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
-
 function App() {
   return (
     <div className="grid-bottom">
-
       <div>
-        <img src="./image/node.png" className="App-logo"  alt=""/>
       </div>
+      <div className="grid-bottom-in">
+        <div>
+          <img src="./image/node.png" className="App-logo" alt="" />
+        </div>
+        <div>
+          <h1>test</h1>
+        </div>
+        <div>
+          <h1>test</h1>
+        </div>
+        <div>
+          <img src="./image/python.png" className="App-logo" alt="" />
+        </div>
+        <div>
+          <img src="./image/unity.png" className="App-logo" alt="" />
+        </div>
+        <div>
+          <h1>test</h1>
+        </div>
+        <div>
+          <h1>test</h1>
+        </div>
+        <div>
+          <img src="./image/git.png" className="App-logo" alt="" />
+        </div>
+
+
+      </div>
+      <div>
+      </div>
+
 
     </div>
   )
